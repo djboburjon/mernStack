78e5023658262c10cd64e67f054525de9a0a58ba
djboburjon <djboburdev@gmail.com>
2025-03-11 00:06:16 +0500

    connected mongoDb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
-const cookieParse = require('cookie-parse');
-const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // create a database connection -> u can also
 // create a separate file for this and then import/use that file here
@@ -12,3 +12,23 @@ mongoose
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "Delete", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
